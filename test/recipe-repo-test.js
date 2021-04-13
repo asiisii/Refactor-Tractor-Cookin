d99f543babd744ec4fbfb9e216797c9fe5191e0e
Ashish Malla <ashishmalla45@gmail.com>
2021-04-13 17:48:37 -0600

    Add test to tag filter method

--- test/recipe-repo-test.js
+++ test/recipe-repo-test.js
@@ -1,23 +1,31 @@
 const chai = require('chai')
 const expect = chai.expect
 
-const RecipeRepository = require('../src/recipe-repo.js')
+import sampleRecipeData from '../src/data/sampledata.js'
+import RecipeRepository from '../src/recipe-repo.js'
 
 describe('Recipe Repository', () => {
-  let recipeRepository
+  let recipeRepository, filteredRecipe
 
   beforeEach(() => {
-    recipeRepository = new RecipeRepository()
+    recipeRepository = new RecipeRepository(sampleRecipeData)
   })
 
-  it('should be a function', () => {
+  it.only('should be a function', () => {
     expect(RecipeRepository).to.be.a('function')
   })
 
-  it('should be an instance of Recipe Repository', () => {
+  it.only('should be an instance of Recipe Repository', () => {
     expect(recipeRepository).to.be.an.instanceof(RecipeRepository)
   })
 
-  
+  it.only('should filter by a tag and return recipe', () => {
+    filteredRecipe = recipeRepository.filterByTag(["antipasti"])
+    expect(filteredRecipe[0].id).to.be.equal(595736)
+    filteredRecipe = recipeRepository.filterByTag(["lunch"])
+    expect(filteredRecipe[0].id).to.be.equal(678353)
+  })
+
+
 
 })
