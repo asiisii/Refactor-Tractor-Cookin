b57104631067e9974005aad27ffdd6a56c658100
Ashish Malla <ashishmalla45@gmail.com>
2021-04-13 15:34:49 -0600

    Initial Commit

---
+++ test/recipe-repo-test.js
@@ -0,0 +1,23 @@
+const chai = require('chai')
+const expect = chai.expect
+
+const RecipeRepository = require('../src/recipe-repo.js')
+
+describe('Recipe Repository', () => {
+  let recipeRepository
+
+  beforeEach(() => {
+    recipeRepository = new RecipeRepository()
+  })
+
+  // it('should be a function', () => {
+  //   expect(RecipeRepository).to.be.a('function')
+  // })
+
+  // it('should be an instance of Recipe Repository', () => {
+  //   expect(recipeRepository).to.be.an.instanceof(RecipeRepository)
+  // })
+
+  
+
+})
