97658cf8d68adaf5d19ad69b1b51fc5ea6cb46db
Melanie Daoheuang <daoh0031@gmail.com>
2021-04-16 06:57:53 -0600

    Remove pantry updates

--- src/pantry.js
+++ src/pantry.js
@@ -2,18 +2,6 @@ class Pantry {
   constructor(userIngredients) {
     this.contents = userIngredients;
   }
-
-  calculateIngredients() {
-
-  }
-
-  calculateMissingIngredients() {
-
-  }
-
-  removeIngredients() {
-    
-  }
 }
 
 export default Pantry;
