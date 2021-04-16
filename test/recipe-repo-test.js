838f68fdb11d60194e9699ff065f74a718fe0029
Ashish Malla <ashishmalla45@gmail.com>
2021-04-16 16:44:25 -0600

    Add test to refactored methods

--- test/recipe-repo-test.js
+++ test/recipe-repo-test.js
@@ -1,7 +1,6 @@
-const chai = require('chai')
-const expect = chai.expect
+import { expect } from 'chai';
 
-import sampleRecipeData from '../src/data/sampledata.js'
+import {sampleRecipeData, sampleIngredientsData} from '../src/data/sampledata.js'
 import RecipeRepository from '../src/recipe-repo.js'
 
 describe('Recipe Repository', () => {
@@ -11,40 +10,48 @@ describe('Recipe Repository', () => {
     recipeRepository = new RecipeRepository(sampleRecipeData)
   })
 
-  it.only('should be a function', () => {
+  it('should be a function', () => {
     expect(RecipeRepository).to.be.a('function')
   })
 
-  it.only('should be an instance of Recipe Repository', () => {
+  it('Should have an array of all recipes', () => {
+    expect(recipeRepository.recipes).to.be.an('array');
+  });
+
+  it('should be an instance of Recipe Repository', () => {
     expect(recipeRepository).to.be.an.instanceof(RecipeRepository)
   })
 
-  it.only('should filter by a tag and return recipe', () => {
-    filteredRecipe = recipeRepository.filterByTag(["antipasti"])
-    expect(filteredRecipe[0].id).to.be.equal(595736)
-    filteredRecipe = recipeRepository.filterByTag(["lunch"])
-    expect(filteredRecipe[0].id).to.be.equal(678353)
-  })
+  describe('findRecipe', () => {
+    it('should filter by a tag and return recipe', () => {
+      filteredRecipe = recipeRepository.getRecipe("antipasti")
+      expect(filteredRecipe[0].id).to.be.equal(595736)
+      filteredRecipe = recipeRepository.getRecipe("lunch")
+      expect(filteredRecipe[0].id).to.be.equal(678353)
+    })
+  
+    it('should filter by multiple tags and return recipe', () => {
+      filteredRecipe = recipeRepository.getRecipe("antipasti", "snack")
+      expect(filteredRecipe[0].id).to.be.equal(595736)
+      filteredRecipe = recipeRepository.getRecipe("lunch", "dinner")
+      expect(filteredRecipe[0].id).to.be.equal(678353)
+    })
+  
+    it('should filter by a name and return recipe', () => {
+      filteredRecipe = recipeRepository.getRecipe("loaded chocolate chip pudding cookie cups")
+      expect(filteredRecipe[0].id).to.be.equal(595736)
+      filteredRecipe = recipeRepository.getRecipe("maple dijon apple cider grilled pork chops")
+      expect(filteredRecipe[0].id).to.be.equal(678353)
+    })
+  
+    it('should filter by an ingredient and return recipe', () => {
+      filteredRecipe = recipeRepository.getRecipe("all purpose flour")
+      expect(filteredRecipe[0].id).to.be.equal(595736)
+      filteredRecipe = recipeRepository.getRecipe("apples")
+      expect(filteredRecipe[0].id).to.be.equal(678353)
+    })
+  });
 
-  it.only('should filter by multiple tags and return recipe', () => {
-    filteredRecipe = recipeRepository.filterByTag(["antipasti", "snack"])
-    expect(filteredRecipe[0].id).to.be.equal(595736)
-    filteredRecipe = recipeRepository.filterByTag(["lunch", "dinner"])
-    expect(filteredRecipe[0].id).to.be.equal(678353)
-  })
-
-  it.only('should filter by a name and return recipe', () => {
-    filteredRecipe = recipeRepository.filterByName("Loaded Chocolate Chip Pudding Cookie Cups")
-    expect(filteredRecipe[0].id).to.be.equal(595736)
-    filteredRecipe = recipeRepository.filterByName("Maple Dijon Apple Cider Grilled Pork Chops")
-    expect(filteredRecipe[0].id).to.be.equal(678353)
-  })
-
-  it.only('should filter by an ingredient and return recipe', () => {
-    filteredRecipe = recipeRepository.filterByIngredient("all purpose flour")
-    expect(filteredRecipe[0].id).to.be.equal(595736)
-    filteredRecipe = recipeRepository.filterByIngredient("apples")
-    expect(filteredRecipe[0].id).to.be.equal(678353)
-  })
+  
 
 })
