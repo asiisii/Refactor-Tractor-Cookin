99f08c584fe92d152e2ea1253cda17e0379c8ba8
Ashish Malla <ashishmalla45@gmail.com>
2021-04-19 16:36:27 -0600

    Fix multi tags search bug

--- src/recipe-repo.js
+++ src/recipe-repo.js
@@ -1,17 +1,26 @@
+import ingredientsData from "./data/ingredients";
+
 class RecipeRepository {
   constructor(data) {
     this.recipes = data;
   }
   
   getRecipe(searchValue) {
-    return this.recipes.filter(recipe => {
-      if (recipe.name.toLowerCase().includes(searchValue) 
-      || recipe.ingredients.find(ingredient => ingredient.name.toLowerCase().includes(searchValue)) 
-      || recipe.tags.every(tag => recipe.tags.includes(searchValue))) {
-        return recipe
-      }
-    })
+    let storeRecipe = [];
+    searchValue.split(' ').forEach(value => 
+      this.recipes.filter(recipe => {
+        if (recipe.name.toLowerCase().includes(value) 
+        || recipe.ingredients.find(ingredient => 
+          ingredient.name.toLowerCase().includes(value)) 
+        || recipe.tags.some(tag => tag.includes(value))) {
+          if (!storeRecipe.includes(recipe)) {
+            storeRecipe.push(recipe)
+          }
+        }
+    }))
+    return storeRecipe; 
   }
+  
 }
 
 export default RecipeRepository;
