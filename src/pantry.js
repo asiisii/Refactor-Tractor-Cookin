a30a7d6e22912c33eb9c7b45f487d1c51af5d3ab
Melanie Daoheuang <daoh0031@gmail.com>
2021-04-19 11:39:17 -0600

    Refactor pantry id method

--- src/pantry.js
+++ src/pantry.js
@@ -1,15 +1,12 @@
 class Pantry {
   constructor(userData) {
     this.contents = userData.pantry;
-    this.ingredientIds;
+    this.ingredientIds = this.getPantryIngredientIds();
     this.needIngredients = [];
   }
 
   getPantryIngredientIds() {
-    const idArray = []
-    this.contents.forEach(item => {
-      idArray.push(item.ingredient);
-    });
+    return this.contents.map(item => item.ingredient)
   }
   //determine user's pantry has enough ingredients to cook meal
   // determine amount missing ingredients needed to cook recipe
