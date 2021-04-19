8d5a79516cc1f35241c9f4b4152c3bc5c4235667
Dustin-Har <harbaughdustin@yahoo.com>
2021-04-19 15:43:29 -0400

    Change layout of scripts.js

--- src/scripts.js
+++ src/scripts.js
@@ -1,37 +1,28 @@
 import './css/base.scss';
 import './css/styles.scss';
-import recipeData from './data/recipes';
 
-// import ingredientsData from './data/ingredients';
-// import users from './data/users';
 import Pantry from './pantry';
-// import Recipe from './recipe';
 import RecipeRepository from './recipe-repo';
 import User from './user';
 
 import { apiData } from './data/fetchedData';
 import domUpdate from './domUpdates';
-// import Cookbook from './cookbook';
-
 
 let favButton = document.querySelector('.view-favorites');
 let homeButton = document.querySelector('.home');
 let searchInput = document.getElementById('search-input');
 let cardArea = document.querySelector('.all-cards');
-// let cookbook = new Cookbook(recipeData);
-// let reciperepo = new RecipeRepository(recipeData);
+
 let user, pantry, reciperepo;
 
 window.onload = onStartup();
-searchInput.addEventListener('keyup', searchRecipe);
 
+searchInput.addEventListener('keyup', searchRecipe);
 homeButton.addEventListener('click', cardButtonConditionals);
 cardArea.addEventListener('click', cardButtonConditionals);
 favButton.addEventListener('click', function () {
   domUpdate.viewFavorites(user)
 });
-
-
 
 function onStartup() {
   apiData()
@@ -48,6 +39,17 @@ function onStartup() {
   });
 }
 
+function cardButtonConditionals(event) {
+  if (event.target.classList.contains('favorite')) {
+    favoriteCard(event);
+  } else if (event.target.classList.contains('card-picture')) {
+    domUpdate.displayDirections(event);
+  } else if (event.target.classList.contains('home')) {
+    favButton.innerHTML = 'View Favorites';
+    domUpdate.populateCards(reciperepo.recipes, user);
+  }
+}
+
 function favoriteCard(event) {
   let specificRecipe = reciperepo.recipes.find(recipe => {
     if (recipe.id  === Number(event.target.id)) {
@@ -61,17 +63,6 @@ function favoriteCard(event) {
   } else if (event.target.classList.contains('favorite-active')) {
     event.target.classList.remove('favorite-active');
     user.removeFromFavorites(specificRecipe)
-  }
-}
-
-function cardButtonConditionals(event) {
-  if (event.target.classList.contains('favorite')) {
-    favoriteCard(event);
-  } else if (event.target.classList.contains('card-picture')) {
-    domUpdate.displayDirections(event);
-  } else if (event.target.classList.contains('home')) {
-    favButton.innerHTML = 'View Favorites';
-    domUpdate.populateCards(reciperepo.recipes, user);
   }
 }
 
